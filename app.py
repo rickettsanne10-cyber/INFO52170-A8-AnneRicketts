9d5db0fbba79af7a7e406067048c8c7b13b0458c
Anne Ricketts <rickeann@shernet.sheridancollege.ca>
2026-08-07 15:06:21 -0400

    Update Docker configuration and Flask app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Fitness Dashboard Application Running in Docker!"
+    return "Fitness Dashboard Docker Container is Running!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
