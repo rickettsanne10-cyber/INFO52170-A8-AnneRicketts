7422588ba4afe2dc73547c486ba120d6cb8b3191
Anne Ricketts <rickeann@shernet.sheridancollege.ca>
2026-08-07 13:32:21 -0400

    Fix CI linting and test configuration

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,11 @@
-from app import add
+import os
+import sys
+
+sys.path.insert(
+    0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+)
+
+from app import add  # noqa: E402
 
 
 def test_add():
